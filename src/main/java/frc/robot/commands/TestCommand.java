5e50388d41eeef502c55abb4a85e4c170eb7879d
AnHonestLawyer <chancestorey@gmail.com>
2020-02-19 16:48:12 -0600

    TestSystem and TestCommand added -For testing specific components

---
+++ src/main/java/frc/robot/commands/TestCommand.java
@@ -0,0 +1,47 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2019 FIRST. All Rights Reserved.                             */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.Constants;
+import frc.robot.RobotContainer;
+import frc.robot.subsystems.TestSubsystem;;
+
+public class TestCommand extends CommandBase {
+  private final TestSubsystem system;
+  /**
+   * Creates a new TestCommand.
+   */
+  public TestCommand() {
+    system = new TestSubsystem();
+    // Use addRequirements() here to declare subsystem dependencies.
+  }
+
+  // Called when the command is initially scheduled.
+  @Override
+  public void initialize() {
+  }
+
+  // Called every time the scheduler runs while the command is scheduled.
+  @Override
+  public void execute() {
+    double stick = RobotContainer.xbox.getRawAxis(Constants.LEFT_STICK_Y);
+    system.moveMotor(stick);
+  }
+
+  // Called once the command ends or is interrupted.
+  @Override
+  public void end(boolean interrupted) {
+  }
+
+  // Returns true when the command should end.
+  @Override
+  public boolean isFinished() {
+    return false;
+  }
+}
